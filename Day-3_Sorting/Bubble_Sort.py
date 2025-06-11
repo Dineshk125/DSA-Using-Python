332a30108865e5e6593c66beeb0f7fd759b8e885
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-11 11:54:21 +0530

    Bubble Sort

--- Day-3_Sorting/Bubble_Sort.py
+++ Day-3_Sorting/Bubble_Sort.py
@@ -31,3 +31,25 @@ def Bubble_sort(nums):
 
 
 print(Bubble_sort(nums))
+
+
+# optimal solutin when list is alrady sorted
+
+
+nums = [3, 4, 5, 6, 7]
+
+
+def Bubble_sort(nums):
+    is_swap = False
+    n = len(nums)
+    for i in range(n - 2, -1, -1):
+        for j in range(0, i + 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                is_swap = True
+    if is_swap == False:
+        return "List is alrady sorted: ", nums
+    return nums
+
+
+print(Bubble_sort(nums))
