2ca4686c6ad5b15ac575e65d38bb35757aab7681
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-16 16:49:38 +0530

    Transpose of Matrix

--- Day-7_Matrix Or 2D Matrix/Transpose Of Matrix.py
+++ Day-7_Matrix Or 2D Matrix/Transpose Of Matrix.py
@@ -1,23 +1,24 @@
-matrix = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 0, 1], [2, 3, 4, 5]]
+matrix = [[0, 1, 2, 3], [4, 5, 6, 7]]
 print(matrix, end=" ")
 print()
 
 row = len(matrix)
 col = len(matrix[0])
 result = [[0] * row for _ in range(0, col)]
-
-for i in range(0, row):
-    for j in range(0, col):
+diff = col - row
+for i in range(0, row - diff):
+    for j in range(0, col - diff):
         result[j][i] = matrix[i][j]
 print(result, end=" ")
 print()
 
-matrix = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 0, 1], [2, 3, 4, 5]]
+matrix = [[0, 1, 2, 3], [4, 5, 6, 7]]
 print(matrix, end=" ")
 print()
 
 row = len(matrix)
 col = len(matrix[0])
-result = [[matrix[j][i] for j in range(0, col)] for i in range(0, row)]
+
+result = [[matrix[j][i] for j in range(0, row)] for i in range(0, col)]
 
 print(result, end=" ")
