34d77e18587af51e3953fb17c5f492359de6c901
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-25 18:07:17 +0530

    Bit manipulation Basic

---
+++ Day-13_Bit_Manipulation/Bit manipulation basic.py
@@ -0,0 +1,12 @@
+"""Introduction to bit manipulation
+
+1. Binary Number Conversion
+2. Diff. Type of Operators -> OR, AND, XOR, NOT, SHIFT
+3. Swapping two Numbers
+4. Check If the I'th Bit set or Not
+5. Extract The I'th Bit
+6. set The I'th Bit
+7. Clear The I'th Bit
+8. Toggle The I'th Bit
+
+"""
