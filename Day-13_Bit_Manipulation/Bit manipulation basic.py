c2ea60c6f807c111bfa8090a0ded99a0a1b59486
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-25 19:35:20 +0530

    Bit manipulation Basic

--- Day-13_Bit_Manipulation/Bit manipulation basic.py
+++ Day-13_Bit_Manipulation/Bit manipulation basic.py
@@ -10,3 +10,22 @@
 8. Toggle The I'th Bit
 
 """
+
+"""
+    1's compliment -> preform flip to find 1's compliment / change value 0->1 and 1->0
+
+    find 1's compliment of (1001)2
+
+        (1001)2 1's compliment -> (0110)2
+
+        
+    2's compliment -> first find 1's compliment and than add 1.
+
+    find 2's compliment of (1101)2
+
+    1. 1's compliment -> (1101)2 - (0010)2
+    2. Then add 1 in 1's compliment
+        (0010)2 -> 0010 + 1 -> 0011
+
+        so the 2's compliment of (1101)2 is (0011)2
+"""
