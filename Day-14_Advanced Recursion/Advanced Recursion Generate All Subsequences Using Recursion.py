c0e7079f84609a3707efa1c149155ef7fff09251
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-26 14:14:33 +0530

    Advanced Recursion | Generate All Subsequences Using Recursion

---
+++ Day-14_Advanced Recursion/Advanced Recursion Generate All Subsequences Using Recursion.py
@@ -0,0 +1,20 @@
+def solve(index, subset):
+    global arr
+    global result
+    n = len(arr)
+    if index >= n:
+        result.append(subset.copy())
+        return
+    subset.append(arr[index])
+    solve(index + 1, subset)
+    subset.pop()
+    solve(index + 1, subset)
+    return result
+
+
+arr = [5, 9, 7]
+subset = []
+index = 0
+result = []
+
+print(solve(index, subset))
