ca95cc59bf955db789623a27f16e9ec7203f6708
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-26 15:53:01 +0530

    Generate Subsequences with Sum K

--- Day-14_Advanced Recursion/Generate Subsequences with Sum K.py
+++ Day-14_Advanced Recursion/Generate Subsequences with Sum K.py
@@ -21,32 +21,33 @@ def backtrack_sub(index, total, subset):
     e = subset.pop()
     summ -= e  ## sum = total
     backtrack_sub(index + 1, summ, subset)
+    return result
 
 
 nums = [5, 9, 0, 4]
 result = []
 target = 9
-backtrack_sub(index=0, total=0, subset=[])
-print(result)
+print(backtrack_sub(index=0, total=0, subset=[]))
+# print(result)
 
 
-# method -> 2, brut
-def backtrack_sub(index, subset):
-    global result
-    global nums
-    global target
-    if index >= len(nums):
-        if sum(subset) == target:
-            result.append(subset.copy())
-            return
-    subset.append(nums[index])
-    backtrack_sub(index + 1, subset)
-    subset.pop()
-    backtrack_sub(index + 1, subset)
+# # method -> 2, brut
+# def backtrack_sub(index, subset):
+#     global result
+#     global nums
+#     global target
+#     if index >= len(nums):
+#         if sum(subset) == target:
+#             result.append(subset.copy())
+#             return
+#     subset.append(nums[index])
+#     backtrack_sub(index + 1, subset)
+#     subset.pop()
+#     backtrack_sub(index + 1, subset)
 
 
-nums = [5, 9, 4, 0]
-result = []
-target = 9
-backtrack_sub(index=0, subset=[])
-print(result)
+# nums = [5, 9, 4, 0]
+# result = []
+# target = 9
+# backtrack_sub(index=0, subset=[])
+# print(result)
