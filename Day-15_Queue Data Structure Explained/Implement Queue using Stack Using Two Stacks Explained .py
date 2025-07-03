f515045d4dffef85f714bf9edf2a74cf5bd8bc30
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-07-03 22:58:07 +0530

    Implement Queue usinf Stach

--- Day-15_Queue Data Structure Explained/Implement Queue using Stack Using Two Stacks Explained .py
+++ Day-15_Queue Data Structure Explained/Implement Queue using Stack Using Two Stacks Explained .py
@@ -20,7 +20,7 @@ class MyQueue(object):
     def pop(self):
         if self.empty():
             return False
-        return self.items.pop(-1)
+        return self.items.pop()
 
     def top(self):
         if self.empty():  # if len(self.items) == 0:
@@ -41,6 +41,57 @@ obj.push(1)
 print(obj.items)
 obj.push(2)
 print(obj.items)
+
+print(obj.top())
+print(obj.pop())
+
+
+"""
+Implementation Queue Usin two Stack.
+
+T.C. = O(N^2)
+S.C.  = O(N)
+
+"""
+
+
+class MyQueue(object):
+
+    def __init__(self):
+        self.st1 = []
+        self.st2 = []
+
+    def push(self, val):
+        while self.st1:
+            self.st2.append(self.st1.pop())
+        self.st1.append(val)
+        while self.st2:
+            self.st1.append(self.st2.pop())
+
+    def pop(self):
+        if self.empty():
+            return False
+        return self.st1.pop()
+
+    def top(self):
+        if self.empty():  # if len(self.items) == 0:
+            return False
+        return self.st1[-1]
+
+    def empty(self):
+        return len(self.st1) == 0
+
+    def size(self):
+        return len(self.st1)
+
+
+obj = MyQueue()
+
+print(obj.st1)
+obj.push(1)
+print(obj.st1)
+obj.push(2)
+print(obj.st1)
 
 print(obj.top())
 print(obj.pop())
