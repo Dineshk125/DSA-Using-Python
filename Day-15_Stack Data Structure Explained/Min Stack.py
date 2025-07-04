2140390dcb7041d2d7e9708bf9ffc5d8a8a85e9a
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-07-04 23:27:16 +0530

    min Stack in python

--- Day-15_Stack Data Structure Explained/Min Stack.py
+++ Day-15_Stack Data Structure Explained/Min Stack.py
@@ -26,7 +26,6 @@ class MinStack(object):
         return self.minstack[-1]
 
 
-# Your MinStack object will be instantiated and called as such:
 obj = MinStack()
 obj.push(10)
 obj.push(20)
@@ -39,3 +38,47 @@ param_3 = obj.top()
 print(param_3)
 param_4 = obj.getMin()
 print(param_4)
+
+
+## Method -> 2
+
+
+class minStack:
+
+    def __init__(self):
+        self.item = []
+
+    def push(self, val):
+        if len(self.item) == 0:
+            self.item.append([val, val])
+        else:
+            mini = min(self.item[-1][1], val)
+            self.item.append([val, mini])
+
+    def pop(self):
+        if len(self.item) == 0:
+            return 0
+        return self.item.pop()
+
+    def top(self):
+        if len(self.item) == 0:
+            return 0
+        return self.item[-1][0]
+
+    def getMin(self):
+        if len(self.item) == 0:
+            return 0
+        return self.item[-1][1]
+
+
+s1 = minStack()
+
+s1.push(100)
+s1.push(200)
+s1.push(300)
+
+print(s1.item)
+s1.pop()
+print(s1.item)
+print(s1.top())
+print(s1.getMin())
